7733ea5dd1b065ea66b36a19108715e16dc803b7
Skillus <patrykstec042003@wp.pl>
2022-12-27 15:14:40 +0100

    Update API.lua

--- API.lua
+++ API.lua
@@ -10,7 +10,7 @@ local Start = tick()
 
 repeat
     task.wait()
-until game:IsLoaded() and game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart") and game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid") and game:GetService("Players").LocalPlayer.Character.Humanoid.Health
+until game.GameId ~= 0 and game:IsLoaded() and game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart") and game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid") and game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").Health > 0
 
 --Notification Library
 
@@ -20,12 +20,8 @@ local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/Iri
 
 --While Loading Script
 
-if getgenv()["Break-Skill_V1_Loaded"] == true then
-    Notification.Notify(
-        "Break-Skill Hub - V1",
-		"<b><font color=\"rgb(255, 30, 30)\">Break-Skill Hub - V1 already executed!</font></b>",
-		"rbxassetid://7771536804",
-		{
+if getgenv()["Break-Skill_Hub_V1_Loaded"] and getgenv()["Break-Skill_Hub_V1_Loaded"].Game then
+    Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\">Break-Skill Hub - V1 Already executed!</font></b>", "rbxassetid://7771536804", {
 			Duration = 10,
 			TitleSettings = {
 				TextXAlignment = Enum.TextXAlignment.Left,
@@ -47,17 +43,28 @@ end
 
 --Instances And Functions
 
+local CoreGui = game:GetService("CoreGui")
 local MarketplaceService = game:GetService("MarketplaceService")
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
-local CoreGui = game:GetService("CoreGui")
 local VirtualUser = game:GetService("VirtualUser")
+local Workspace = game:GetService("Workspace")
 
 local Client = Players.LocalPlayer
 
-getgenv()["DH"] = true
-getgenv()["PL"] = true
-getgenv()["TWW"] = true
+local QueueOnTeleport = queue_on_teleport or (syn and syn.queue_on_teleport)
+
+local LoadArgs = {
+    ...
+}
+
+Client.Idled:Connect(function()
+    VirtualUser:Button2Down(Vector2.new(0, 0), Workspace.CurrentCamera.CFrame)
+
+    task.wait(1)
+
+    VirtualUser:Button2Up(Vector2.new(0, 0), Workspace.CurrentCamera.CFrame)
+end)
 
 RunService.RenderStepped:Connect(function()
     for i, v in pairs(CoreGui:GetChildren()) do
@@ -80,50 +87,99 @@ elseif getexecutorname then
 elseif KRNL_LOADED then
 
 else
-    Client:Kick("\n[Break-Skill Hub - V1]:\n[Executor Error]:\nYour executor is not supported!\nYou can find supported executors on our discord!\ndiscord.gg/ev8bxrAa9p (Copied)")
-
-    setclipboard("discord.gg/ev8bxrAa9p")
+    Client:Kick("\n[Break-Skill Hub - V1]:\n[Executor Error]:\nYour executor is not supported!\nYou can find supported executors on our discord!\ndiscord.gg/ev8bxrAa9p")
 
     return
 end
 
---Anti AFK
+local function GetSupportedGame()
+    local Game
 
-Client.Idled:Connect(function()
-    VirtualUser:Button2Down(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
+    for id, info in pairs(getgenv()["Break-Skill_Hub_V1_Loaded"].Games) do
+        if tostring(game.GameId) == id then
+            Game = info
 
-    task.wait(1)
+            break
+        end
+    end
 
-    VirtualUser:Button2Up(Vector2.new(0, 0), workspace.CurrentCamera.CFrame)
-end)
+    if not Game then
+        return getgenv()["Break-Skill_Hub_V1_Loaded"].Games.Universal
+    end
+
+    return Game
+end
+
+local function Concat(array, separator)
+    local Ouput = ""
+
+    for i, v in pairs(array) do
+        Ouput = i == #array and Ouput .. tostring(v) or Ouput .. tostring(v) .. separator
+    end
+
+    return Ouput
+end
+
+local function GetScript(script)
+    return getgenv()["Break-Skill_Hub_V1_Loaded"].Debug and readfile("Break-Skill Hub - V1/" .. script .. ".lua") or game:HttpGetAsync(("%s%s.lua"):format(getgenv()["Break-Skill_Hub_V1_Loaded"].Domain, script))
+end
+
+local function LoadScript(script)
+    return loadstring(getgenv()["Break-Skill_Hub_V1_Loaded"].Debug and readfile("Break-Skill Hub - V1/" .. script .. ".lua") or game:HttpGetAsync(("%s%s.lua"):format(getgenv()["Break-Skill_Hub_V1_Loaded"].Domain, script)))
+end
 
 --[
 --Game Script Loading
 --]
 
-if game.PlaceId == 142823291 and getgenv()["DH"] == true then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Sklllus/Break-Skill-Hub-V1/main/Murder%20Mystery%202.lua"))()
+getgenv()["Break-Skill_Hub_V1_Loaded"] = {
+    Debug = LoadArgs[1],
+    Utilities = {},
+    Domain = "https://raw.githubusercontent.com/Sklllus/B-S-Hub-V1/main/",
+    Games = {
+        ["Universal"] = {
+            Name = "Universal",
+            Script = "Universal"
+        },
+        ["1054526971"] = {
+            Name = "Blackhawk Rescue Mission 5",
+            Script = "Games/BlackHawkRescueMission5"
+        },
+        ["2788229376"] = {
+            Name = "Da Hood",
+            Script = "Games/DaHood"
+        },
+        ["443406476"] = {
+            Name = "Project Lazarus",
+            Script = "Games/ProjectLazarus"
+        }
+    }
+}
 
-    if getgenv()["Break-Skill_Hub_DaHood_Loaded"] == true then
-        local End = (tick() - Start)
+getgenv()["Break-Skill_Hub_V1_Loaded"].UI = LoadScript("Utilities/UI")
+getgenv()["Break-Skill_Hub_V1_Loaded"].Misc = LoadScript("Utilities/Misc")
+getgenv()["Break-Skill_Hub_V1_Loaded"].Drawing = LoadScript("Utilities/Drawing")
 
-        Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\">Loading script time: </font><font color=\"rgb(30, 255, 30)\">" .. End .. " </font><font color=\"rgb(255, 30, 30)\">ms.</font></b>", "rbxassetid://7771536804", {
-			Duration = 10,
-			TitleSettings = {
-				TextXAlignment = Enum.TextXAlignment.Left,
-				Font = Enum.Font.SourceSansBold
-			},
-			GradientSettings = {
-				GradientEnabled = false,
-				SolidColorEnabled = true,
-				SolidColor = Color3.fromRGB(255, 30, 30),
-				Retract = true
-			}
-		})
+local SupportedGame = GetSupportedGame()
+
+Client.OnTeleport:Connect(function(teleportState)
+    if teleportState == Enum.TeleportState.InProgress then
+        QueueOnTeleport(([[local LoadArgs = {
+            %s
+        }
+
+        loadstring(LoadArgs[1] and readfile(%sLoader.lua))(unpack(LoadArgs))]]):format(Concat(LoadArgs, ","), getgenv()["Break-Skill_Hub_V1_Loaded"].Domain))
     end
+end)
 
-    if getgenv()["Break-Skill_Hub_Loaded"] == true then
-        Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\">Successfully loaded script for</font> <font color=\"rgb(30, 255, 30)\">" .. MarketplaceService:GetProductInfo(game.PlaceId).Name .. "</font><font color=\"rgb(255, 30, 30)\">!</font></b>", "rbxassetid://7771536804", {
+if SupportedGame then
+    getgenv()["Break-Skill_Hub_V1_Loaded"].Game = SupportedGame.Name
+
+    LoadScript(SupportedGame.Script)
+
+    local End = (tick() - Start)
+
+    Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\"><b><font color=\"rgb(255, 30, 30)\">Loading script time: </font><font color=\"rgb(30, 255, 30)\">" .. End .. " </font><font color=\"rgb(255, 30, 30)\">ms.</font></b>", "rbxassetid://7771536804", {
 			Duration = 10,
 			TitleSettings = {
 				TextXAlignment = Enum.TextXAlignment.Left,
@@ -135,15 +191,10 @@ if game.PlaceId == 142823291 and getgenv()["DH"] == true then
 				SolidColor = Color3.fromRGB(255, 30, 30),
 				Retract = true
 			}
-		})
-    end
-elseif game.PlaceId == 443406476 and getgenv()["PL"] == true then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Sklllus/Break-Skill-Hub-V1/main/Project%20Lazarus.lua"))()
+		}
+	)
 
-	if getgenv()["Break-Skill_Hub_ProjectLazarus_Loaded"] == true then
-		local End = (tick() - Start)
-
-		Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\">Loading script time: </font><font color=\"rgb(30, 255, 30)\">" .. End .. " </font><font color=\"rgb(255, 30, 30)\">ms.</font></b>", "rbxassetid://7771536804", {
+    Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\">Successfully loaded script for</font> <font color=\"rgb(30, 255, 30)\">" .. MarketplaceService:GetProductInfo(game.PlaceId).Name .. "</font><font color=\"rgb(255, 30, 30)\">!</font></b>", "rbxassetid://7771536804", {
 			Duration = 10,
 			TitleSettings = {
 				TextXAlignment = Enum.TextXAlignment.Left,
@@ -155,82 +206,6 @@ elseif game.PlaceId == 443406476 and getgenv()["PL"] == true then
 				SolidColor = Color3.fromRGB(255, 30, 30),
 				Retract = true
 			}
-		})
-	end
-
-	if getgenv()["Break-Skill_Hub_Loaded"] == true then
-		Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\">Successfully loaded script for</font> <font color=\"rgb(30, 255, 30)\">" .. MarketplaceService:GetProductInfo(game.PlaceId).Name .. "</font><font color=\"rgb(255, 30, 30)\">!</font></b>", "rbxassetid://7771536804", {
-			Duration = 10,
-			TitleSettings = {
-				TextXAlignment = Enum.TextXAlignment.Left,
-				Font = Enum.Font.SourceSansBold
-			},
-			GradientSettings = {
-				GradientEnabled = false,
-				SolidColorEnabled = true,
-				SolidColor = Color3.fromRGB(255, 30, 30),
-				Retract = true
-			}
-		})
-	end
-elseif game.PlaceId == 2317712696 and getgenv()["TWW"] == true then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Sklllus/Break-Skill-Hub-V1/main/The%20Wild%20West.lua"))()
-
-	if getgenv()["Break-Skill_Hub_TheWildWest_Loaded"] == true then
-		local End = (tick() - Start)
-
-		Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\">Loading script time: </font><font color=\"rgb(30, 255, 30)\">" .. End .. " </font><font color=\"rgb(255, 30, 30)\">ms.</font></b>", "rbxassetid://7771536804", {
-			Duration = 10,
-			TitleSettings = {
-				TextXAlignment = Enum.TextXAlignment.Left,
-				Font = Enum.Font.SourceSansBold
-			},
-			GradientSettings = {
-				GradientEnabled = false,
-				SolidColorEnabled = true,
-				SolidColor = Color3.fromRGB(255, 30, 30),
-				Retract = true
-			}
-		})
-	end
-
-	if getgenv()["Break-Skill_Hub_Loaded"] == true then
-		Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\">Successfully loaded script for</font> <font color=\"rgb(30, 255, 30)\">" .. MarketplaceService:GetProductInfo(game.PlaceId).Name .. "</font><font color=\"rgb(255, 30, 30)\">!</font></b>", "rbxassetid://7771536804", {
-			Duration = 10,
-			TitleSettings = {
-				TextXAlignment = Enum.TextXAlignment.Left,
-				Font = Enum.Font.SourceSansBold
-			},
-			GradientSettings = {
-				GradientEnabled = false,
-				SolidColorEnabled = true,
-				SolidColor = Color3.fromRGB(255, 30, 30),
-				Retract = true
-			}
-		})
-	end
-elseif game.PlaceId == 142823291 and getgenv()["DH"] == false then
-    Client:Kick("\n[Break-Skill Hub - V1]:\n[Game Error]:\nGame disabled!\nYou can find enabled games on our discord!\ndiscord.gg/ev8bxrAa9p (Copied)")
-
-    setclipboard("discord.gg/ev8bxrAa9p")
-
-    return
-elseif game.PlaceId == 443406476 and getgenv()["PL"] == false then
-	Client:Kick("\n[Break-Skill Hub - V1]:\n[Game Error]:\nGame disabled!\nYou can find enabled games on our discord!\ndiscord.gg/ev8bxrAa9p (Copied)")
-
-	setclipboard("discord.gg/ev8bxrAa9p")
-
-	return
-elseif game.PlaceId == 2317712696 and getgenv()["TWW"] == false then
-	Client:Kick("\n[Break-Skill Hub - V1]:\n[Game Error]:\nGame disabled!\nYou can find enabled games on our discord!\ndiscord.gg/ev8bxrAa9p (Copied)")
-
-	setclipboard("discord.gg/ev8bxrAa9p")
-
-	return
-else
-    Client:Kick("\n[Break-Skill Hub - V1]:\n[Game Error]:\nGame not supported!\nYou can find supported games on our discord!\ndiscord.gg/ev8bxrAa9p (Copied)")
-
-    setclipboard("discord.gg/ev8bxrAa9p")
-
-    return
+		}
+	)
 end
