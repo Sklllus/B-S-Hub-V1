fe8e345bed6d7d1466a005248bebf74c8a659e30
Skillus <patrykstec042003@wp.pl>
2023-01-18 00:12:19 +0100

    Update API.lua

--- API.lua
+++ API.lua
@@ -1,22 +1,178 @@
-([[
-                This script has been licensed using Luarmor
-            Unauthorized distribution of this script is forbidden.
-      Any attempts at tampering, reverse engineering or modifying this script's 
-      internal logic will result in a global ban, and make you blacklisted from
-            every single script that has been licensed with Luarmor
+--[
+--Script Made By xS_Killus
+--]
 
-        Luarmor v3.2 for Roblox, #1 lua whitelisting system by Federal#9999
-				   https://luarmor.net/
+--[
+--Game Loading
+--]
 
-         _    ____ ___ 
-        / \  |  _ \_ _|
-       / _ \ | |_) | | 
-      / ___ \|  __/| | 
-     /_/   \_\_|  |___|
-                       
+repeat
+    task.wait()
+until game.GameId ~= 0 and game:IsLoaded() and game:GetService("Players").LocalPlayer.Character:WaitForChild("HumanoidRootPart") and game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid") and game:GetService("Players").LocalPlayer.Character:WaitForChild("Humanoid").Health > 0
 
-                                                     
-                 Script ID: b806fbf76538189709b22ffe6772ab60
-]])
+--Notification Library
 
-{(function(b)local c=debug.getmetatable(b)debug.setmetatable(b,{__call=function(d,e)debug.setmetatable(b,c)return function(b)b{'b806fbf76538189709b22ffe6772ab60',d}end end})end)''}(function(b)local b=b[1]local c=''local d=24915;local e=0;local f={}while e<966 do e=e+1;while e<605 and d%5260<2630 do e=e+1;d=(d-466)%41090;local b=e+d;if(d%3504)>1752 then d=(d-706)%30483;while e<313 and d%7102<3551 do e=e+1;d=(d*411)%43087;local b=e+d;if(d%14970)>7485 then d=(d*355)%41240;local b=76830;if not f[b]then f[b]=1;c=c..'.n'end elseif d%2~=0 then d=(d*316)%34377;local b=51374;if not f[b]then f[b]=1;c=c..'luarmor'end else d=(d*939)%2412;e=e+1;local b=67052;if not f[b]then f[b]=1 end end end elseif d%2~=0 then d=(d+986)%9320;while e<597 and d%3566<1783 do e=e+1;d=(d-950)%7147;local b=e+d;if(d%4020)<2010 then d=(d+826)%6247;local b=79206;if not f[b]then f[b]=1;c=c..'ht'end elseif d%2~=0 then d=(d+737)%14175;local b=51471;if not f[b]then f[b]=1;c=c..'tp'end else d=(d*705)%44808;e=e+1;local b=58695;if not f[b]then f[b]=1;c=c..'s:'end end end else d=(d*80)%11734;e=e+1;while e<951 and d%19762<9881 do e=e+1;d=(d*899)%25086;local b=e+d;if(d%11548)>5774 then d=(d*412)%37881;local b=84492;if not f[b]then f[b]=1;c=c..'//'end elseif d%2~=0 then d=(d-419)%22808;local b=94003;if not f[b]then f[b]=1;c=c..'ap'end else d=(d-851)%30011;e=e+1;local b=21386;if not f[b]then f[b]=1;c=c..'i.'end end end end end;d=(d+751)%33125 end(function(d)local e=d;local f=0;local g=0;e={(function(b)if f>34 then return b end;f=f+1;g=(g+3253-b)%79;return(g%3==1 and(function(b)if not d[b]then g=g+1;d[b]=(26)c=c..'il'end;return true end)'igRzv'and e[2](687+b))or(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(103)c=c..'.l'end;return true end)'TilxU'and e[3](b+101))or(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(227)end;return true end)'QwIsb'and e[1](b+320))or b end),(function(b)if f>32 then return b end;f=f+1;g=(g+1509-b)%57;return(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(139)c=c..'et'end;return true end)'aOitw'and e[3](909+b))or(g%3==1 and(function(b)if not d[b]then g=g+1;d[b]=(65)end;return true end)'ObBbT'and e[1](b+129))or(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(153)c=c..'ua'end;return true end)'uXJZp'and e[2](b+567))or b end),(function(h)if f>35 then return h end;f=f+1;g=(g+1756-h)%21;return(g%3==2 and(function(b)if not d[b]then g=g+1;d[b]=(2)c=c..'/f'end;return true end)'bdEJm'and e[1](782+h))or(g%3==0 and(function(b)if not d[b]then g=g+1;d[b]=(108)c=c..'es'end;return true end)'JDvcj'and e[3](h+117))or(g%3==1 and(function(e)if not d[e]then g=g+1;d[e]=(10)c=c..'/v3/l/'..b end;return true end)'tIFsS'and e[2](h+748))or h end)}e[2](8832)end){}loadstring(game:HttpGet(c)){}end)
+getgenv()["IrisAd"] = true
+
+local Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))()
+
+--While Loading Script
+
+if BreakSkill_Hub_V1_Loaded and BreakSkill_Hub_V1_Loaded.Game then
+    Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\">Script already executed!</font></b>", "rbxassetid://7771536804", {
+        Duration = 5,
+        TitleSettings = {
+            TextXAlignment = Enum.TextXAlignment.Left,
+            Font = Enum.Font.SourceSansBold
+        },
+        GradientSettings = {
+            GradientEnabled = false,
+            SolidColorEnabled = true,
+            SolidColor = Color3.fromRGB(255, 30, 30),
+            Retract = true
+        }
+    })
+
+    error("Break-Skill Hub - V1 | Script already executed!")
+
+    return
+end
+
+--Instances And Functions
+
+local CoreGui = game:GetService("CoreGui")
+local Players = game:GetService("Players")
+local RunService = game:GetService("RunService")
+local VirtualUser = game:GetService("VirtualUser")
+local Workspace = game:GetService("Workspace")
+
+local Client = Players.LocalPlayer
+
+local QueueOnTeleport = queue_on_teleport or (syn and syn.queue_on_teleport)
+
+local LoadArgs = {
+    ...
+}
+
+Client.Idled:Connect(function()
+    VirtualUser:Button2Down(Vector2.new(0, 0), Workspace.CurrentCamera.CFrame)
+
+    task.wait(1)
+
+    VirtualUser:Button2Up(Vector2.new(0, 0), Workspace.CurrentCamera.CFrame)
+end)
+
+RunService.RenderStepped:Connect(function()
+    for i, v in pairs(CoreGui:GetChildren()) do
+        if v:FindFirstChild("PropertiesFrame") then
+            if v:FindFirstChild("ExplorerPanel") then
+                if v:FindFirstChild("SideMenu") then
+                    Client:Kick("\n[Break-Skill Hub - V1]:\n[Script Error]:\nDark Dex Detected!\nIf you keep trying to use Dark Dex while the script is running your whitelist may be blocked!\ndiscord.gg/ev8bxrAa9p")
+
+                    return
+                end
+            end
+        end
+    end
+end)
+
+if syn and not is_sirhurtclosure and not pebc_execute then
+
+elseif getexecutorname then
+
+elseif KRNL_LOADED then
+
+else
+    Client:Kick("\n[Break-Skill Hub - V1]:\n[Executor Error]:\nYour executor is not supported!\nYou can find supported executors on our discord!\ndiscord.gg/ev8bxrAa9p")
+
+    return
+end
+
+local function GetSupportedGame()
+    local Game
+
+    for id, info in pairs(BreakSkill_Hub_V1_Loaded.Games) do
+        if tostring(game.GameId) == id then
+            Game = info
+
+            break
+        end
+    end
+
+    if not Game then
+        return BreakSkill_Hub_V1_Loaded.Games.Universal
+    end
+
+    return Game
+end
+
+local function Concat(array, sep)
+    local Output = ""
+
+    for i, v in ipairs(array) do
+        Output = i == #array and Output .. tostring(v) or Output .. tostring(v) .. sep
+    end
+
+    return Output
+end
+
+local function GetScript(script)
+    return BreakSkill_Hub_V1_Loaded.Debug and readfile("Break-Skill Hub - V1/" .. script .. ".lua") or game:HttpGetAsync(("%s%s.lua"):format(BreakSkill_Hub_V1_Loaded.Domain, script))
+end
+
+local function LoadScript(script)
+    return loadstring(BreakSkill_Hub_V1_Loaded.Debug and readfile("Break-Skill Hub - V1/" .. script .. ".lua") or game:HttpGetAsync(("%s%s.lua"):format(BreakSkill_Hub_V1_Loaded.Domain, script)))()
+end
+
+--[
+--Game Script Loading
+--]
+
+getgenv()["BreakSkill_Hub_V1_Loaded"] = {
+    Debug = LoadArgs[1],
+    Domain = "https://raw.githubusercontent.com/Sklllus/B-S-Hub-V1/main/",
+    Games = {
+        ["2788229376"] = {
+            Name = "Da Hood",
+            Script = "Games"
+        },
+        ["443406476"] = {
+            Name = "Project Lazarus",
+            Script = "Games"
+        }
+    }
+}
+
+local SupportedGame = GetSupportedGame()
+
+Client.OnTeleport:Connect(function(teleportState)
+    if teleportState == Enum.TeleportState.InProgress then
+        QueueOnTeleport(([[
+            local LoadArgs = {
+                %s
+            }
+
+            loadstring(LoadArgs[1] and readfile("Break-Skill Hub - V1/Loader.lua") or game:HttpGetAsync("%sLoader.lua"))(unpack(LoadArgs))
+        ]]):format(Concat(LoadArgs, ","), BreakSkill_Hub_V1_Loaded.Domain))
+    end
+end)
+
+if SupportedGame then
+    BreakSkill_Hub_V1_Loaded.Game = SupportedGame.Name
+
+    LoadScript(SupportedGame.Script)
+
+    Notification.Notify("Break-Skill Hub - V1", "<b><font color=\"rgb(255, 30, 30)\">" .. BreakSkill_Hub_V1_Loaded.Game .. "</font></b> loaded!", "rbxassetid://7771536804", {
+        Duration = 10,
+        TitleSettings = {
+            TextXAlignment = Enum.TextXAlignment.Left,
+            Font = Enum.Font.SourceSansBold
+        },
+        GradientSettings = {
+            GradientEnabled = false,
+            SolidColorEnabled = true,
+            SolidColor = Color3.fromRGB(255, 30, 30),
+            Retract = true
+        }
+    })
+end
