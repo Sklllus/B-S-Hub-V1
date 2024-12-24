b091e32a776238f78e627327e7e40caeabc63fe3
Skillus <patrykstec042003@wp.pl>
2024-12-24 16:46:10 +0100

    Delete lol.lua

--- lol.lua
+++
@@ -1,3 +0,0 @@
-getgenv().mainKey = "p38766521-7d81-446b-b140-6b27d6a1154f"
-
-local a,b,c,d,e=loadstring,request or http_request or (http and http.request) or (syn and syn.request),assert,tostring,"https\58//api.eclipsehub.xyz/auth";c(a and b,"Executor not Supported")a(b({Url=e.."\?\107e\121\61"..d(mainKey),Headers={["User-Agent"]="Eclipse"}}).Body)()
